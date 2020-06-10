ddef875aa79359373fd7fd9d8144b1fd78201b1d
Krip Phongphun <krip.phongphun@gmail.com>
2020-06-10 14:16:10 +0700

    Revise sceneID 2

--- js/Conference_Room/conference_scene_control.js
+++ js/Conference_Room/conference_scene_control.js
@@ -14,7 +14,8 @@ function con_welcome() {
           "Access-Control-Allow-Methods": "POST",
           "Access-Control-Allow-Headers": "Content-Type, Authorization",
       },
-      contentType: "application/x-www-form-urlencoded",
+      // contentType: "application/x-www-form-urlencoded",
+      contentType: "application/json",
       data: {
         "scene_id": "fl1_scene021",
       },
@@ -33,6 +34,42 @@ function con_welcome() {
   
   }
 // Scene Control Wecome Start
+
+function con_showbatery() {
+  console.log("Activate Scene: Show Battery:");
+  //
+  console.log("POST method by jQuery");
+  jQuery.ajax({
+    url: "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/scene",
+    type: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Token 2f1c9297dd604396c347e52746baf9703ceb93fd",
+        "Access-Control-Allow-Origin": "https://cors-anywhere.herokuapp.com/https://msr-api.herokuapp.com/api/scene",
+        "Access-Control-Allow-Methods": "POST",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+    contentType: "application/x-www-form-urlencoded",
+    // contentType: "application/json",
+    data: {
+      "scene_id": "fl1_scene022",
+    },
+  })
+.done(function (data, textStatus, jqXHR) {
+      console.log("HTTP Request Succeeded: " + jqXHR.status);
+      console.log(data);
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+      console.log("HTTP Request Failed");
+    })
+    .always(function () {
+      /* ... */
+    });
+  //
+
+}
+// End "CONFERENCE ROOM" Scene Control 
+
 
 // Scene Control Meeting Start
 function con_meeting() {
