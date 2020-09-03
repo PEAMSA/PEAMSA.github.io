be95c8229f3894a32070e5d666153f8a14c8960a
Krip Phongphun <krip.phongphun@gmail.com>
2020-09-03 17:40:14 +0700

    Edit get data from firebase

--- js/1st_floor.js/energyFloor1.js
+++ js/1st_floor.js/energyFloor1.js
@@ -10,7 +10,7 @@ ref_kW_Lighting_1.on("value", function(snapshot) {
     var kW_Lighting_1 = snapshot.val();
     console.log(kW_Lighting_1);
     // var kW_Lighting_1 = parseInt(kW_Lighting_1)*-1;
-    var kW_Lighting_1 = parseFloat(kW_Lighting_1)*-1;
+    var kW_Lighting_1 = parseFloat(kW_Lighting_1);
     document.getElementById("kW_Lighting_1").innerHTML = kW_Lighting_1;
     localStorage.setItem("kW_Lighting_1_local", kW_Lighting_1)
   });
@@ -19,7 +19,7 @@ ref_kW_Outlet_1.on("value", function(snapshot) {
     console.log("Get kW_Outlet_1 from firebase")
     //  console.log(snapshot.val());
     var kW_Outlet_1 = snapshot.val();
-    var kW_Outlet_1 = parseFloat(kW_Outlet_1)*-1;
+    var kW_Outlet_1 = parseFloat(kW_Outlet_1);
     // console.log("Temp is " + temp_server);
     document.getElementById("kW_Outlet_1").innerHTML = kW_Outlet_1;
     localStorage.setItem("kW_Outlet_1_local", kW_Outlet_1)
@@ -29,7 +29,7 @@ ref_kW_Aircon_1.on("value", function(snapshot) {
     console.log("Get kW_Aircon_1 from firebase")
     //  console.log(snapshot.val());
     var kW_Aircon_1 = snapshot.val();
-    var kW_Aircon_1 = parseFloat(kW_Aircon_1)*-1;
+    var kW_Aircon_1 = parseFloat(kW_Aircon_1);
     // console.log("Temp is " + temp_server);
     document.getElementById("kW_Aircon_1").innerHTML = kW_Aircon_1;
     localStorage.setItem("kW_Aircon_1_local", kW_Aircon_1)
